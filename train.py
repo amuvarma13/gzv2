d1188fda54001a9730102ffa9dbc7e19a5e1bc3c
Amartya Varma <amartya.varma@gmail.com>
2024-11-22 10:02:50 -0800

    f

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import transformers
 from transformers import Trainer, TrainingArguments
 import torchaudio
 
-from gazelle.gazelle.modeling_gazelle import (
+from gazelle.gazelle import (
     GazelleConfig,
     GazelleForConditionalGeneration,
     GazelleProcessor,

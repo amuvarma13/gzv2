cad6d7063abc9cdec2b29e890e842dd0e200c845
Amartya Varma <amartya.varma@gmail.com>
2024-11-24 21:20:47 -0800

    f

--- import_check.py
+++ import_check.py
@@ -1,7 +1,9 @@
 import torch
 import torchaudio
 import flash_attn
+import transformers
 
 print(f"PyTorch version: {torch.__version__}")
 print(f"torchaudio version: {torchaudio.__version__}")
 print(f"flash_attn version: {flash_attn.__version__}")
+print(f"transformers version: {transformers.__version__}")

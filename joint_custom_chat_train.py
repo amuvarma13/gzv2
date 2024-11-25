7092b8951e61b63bc0c96173d7bdad3738d1e573
Amartya Varma <amartya.varma@gmail.com>
2024-11-25 11:10:05 -0800

    f

---
+++ joint_custom_chat_train.py
@@ -0,0 +1,226 @@
+from datasets import Dataset
+
+
+from transformers import Wav2Vec2Config, LlamaConfig
+import torch
+import transformers
+from transformers import Trainer, TrainingArguments
+import torchaudio
+from peft import get_peft_model, LoraConfig, TaskType
+
+
+#trainable params: 7258112 || all params: 7376934784 || trainable%: 0.0983892661724797
+
+
+from gzf import (
+    GazelleConfig,
+    GazelleForConditionalGeneration,
+    GazelleProcessor,
+)
+
+import wandb
+
+
+device = "cpu"
+dtype= torch.float32
+if torch.cuda.is_available():
+    device = "cuda"
+    dtype = torch.bfloat16
+    print(f"Using {device} device")
+elif torch.backends.mps.is_available():
+    device = "mps"
+    dtype = torch.float16
+    print(f"Using {device} device")
+
+
+
+model_id = "amuvarma/convo-tts-tune-7contentonly"
+# model_id = "meta-llama/Llama-3.2-3B-Instruct"
+
+
+config = GazelleConfig(
+    audio_model_id="facebook/wav2vec2-base-960h", 
+    text_model_id=model_id, 
+    audio_token_index = 134411, 
+    vocab_size=134411,
+
+    )
+
+model = GazelleForConditionalGeneration(config).to(dtype=dtype)
+
+tokenizer = transformers.AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
+number_add_tokens = 6 * 1024 + 10
+new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]
+tokenizer.add_tokens(new_tokens)
+tokenizer.add_special_tokens({'additional_special_tokens': ['<|audio|>']})
+# Don't forget to resize model embeddings if you have a model:
+print("model device", model.device)
+model.resize_token_embeddings(len(tokenizer))
+
+
+wandb.init(
+    project="colab-a100-40gb",
+    name = "vmllamaspeechcontentonly-500k-8h100s-3"
+    )
+
+file_path = 'transcribe_exps.txt'
+
+try:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        expressions = [line.strip() for line in file if line.strip()]
+except FileNotFoundError:
+    print(f"The file {file_path} does not exist.")
+except IOError:
+    print(f"An error occurred while reading the file {file_path}.")
+
+
+from datasets import load_dataset
+# dsn = "amuvarma/mls-eng-10k-500k"
+dsn = "amuvarma/mls-eng-10k-dev-3k"
+ds = load_dataset(dsn, split="dev")
+
+dataset = ds
+
+
+
+
+model = model.to(dtype=dtype)
+
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    r=8,
+    lora_alpha=32,
+    lora_dropout=0.1,
+    bias="none",
+    target_modules=["q_proj", "v_proj", "k_proj", "o_proj" ]
+)
+
+# Apply LoRA to the model
+model = get_peft_model(model, peft_config)
+
+for name, param in model.named_parameters():
+    if "audio_tower" in name:
+        param.requires_grad = True
+    if "multi_modal_projector" in name:
+        param.requires_grad = True
+trainable_params = 0
+all_param = 0
+for _, param in model.named_parameters():
+    all_param += param.numel()
+    if param.requires_grad:
+        trainable_params += param.numel()
+print(f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}")
+
+
+
+
+
+trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+all_params = sum(p.numel() for p in model.parameters())
+
+print(f"\nTrainable parameters: {trainable_params:,}")
+print(f"All parameters: {all_params:,}")
+
+audio_processor = transformers.Wav2Vec2Processor.from_pretrained(
+    "facebook/wav2vec2-base-960h"
+)
+
+
+
+print("creating collator")
+
+
+def inference_collator(audio_input, ass_res, instruction="Transcribe the following \n<|audio|>"):
+    audio_values = audio_processor(
+        audio=audio_input, return_tensors="pt", sampling_rate=16000
+    ).input_values
+
+    user_phrase = "<|audio|>"
+    user_input_ids = tokenizer(user_phrase, return_tensors="pt").input_ids
+    assistant_input_ids = tokenizer(ass_res, return_tensors="pt").input_ids
+
+    # print("user_input_ids", user_input_ids.shape)
+
+    # input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+
+    start_token = torch.tensor([[128259]], dtype=torch.int64)
+    end_tokens = torch.tensor([[128009, 128260, 128261]], dtype=torch.int64)
+    final_tokens = torch.tensor([[128009]], dtype=torch.int64)
+
+    user_tokens = torch.cat([start_token, user_input_ids, end_tokens], dim=1)
+
+
+    labels = torch.cat([start_token, user_input_ids, end_tokens, assistant_input_ids, final_tokens], dim=1)
+
+    true_labels = torch.full_like(labels, -100)
+    true_labels[:, user_tokens.shape[1]:] = labels[:, user_tokens.shape[1]:]
+
+    # print("true_labels", true_labels)
+    # print("input_ids", labels)
+
+    attention_mask = torch.ones_like(labels)
+
+
+    return {
+        "audio_values": audio_input.to(model.device).to(model.dtype),
+        "input_ids": labels.to(model.device),
+        "labels": true_labels.to(model.device),
+        "attention_mask": attention_mask.to(model.device)
+    }
+
+print("creating data collator")
+
+import random
+class AudioChatDataCollator:
+    def __init__(self, instruction="Transcribe the following \n<|audio|>"):
+        self.instruction = instruction
+
+    def __call__(self, features):
+        audio = torch.tensor([features[0]["audio"]["array"]])
+        assistant_response = features[0]["transcript"]
+
+        random_expression = random.choice(expressions)
+
+
+
+        batch = inference_collator(audio, assistant_response, random_expression)
+
+        return {
+            "audio_values": batch["audio_values"].cpu(),
+            "input_ids": batch["input_ids"].cpu(),
+            "labels": batch["labels"].cpu(),
+            "attention_mask": batch["attention_mask"].cpu()
+        }
+    
+
+
+print("creating trainer")
+
+training_args = TrainingArguments(
+    output_dir="./audio-chat-test",
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=2,  # Changed to 16
+    num_train_epochs=1,
+    learning_rate=2e-3,  # Changed to 2*10^-3
+    save_strategy="no",
+    logging_steps=1,
+    evaluation_strategy="no",
+    report_to="wandb",
+    push_to_hub=False,
+    dataloader_pin_memory=False,
+    remove_unused_columns=False,
+    warmup_ratio=0.03,
+    lr_scheduler_type="cosine",
+    bf16=True
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=AudioChatDataCollator(),
+)
+
+print("training")
+
+trainer.train()

bf5c827d53800cc1cab721b2dd686250e70bc7bd
Amartya Varma <amartya.varma@gmail.com>
2024-11-30 18:12:49 -0800

    f

--- functional_train_joint.py
+++ functional_train_joint.py
@@ -94,8 +94,8 @@ for param in model.parameters():
 # Then unfreeze just the multi_modal_projector
 # First set requires_grad
 for name, param in model.named_parameters():
-    if "multi_modal_projector" in name:
-        param.requires_grad = True
+    # if "multi_modal_projector" in name:
+    #     param.requires_grad = True
     if "language_model" in name:
         param.requires_grad = True
 #         torch.nn.init.normal_(param, mean=0.0, std=0.02)

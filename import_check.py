aabf6813cace1c52f123dd44ccf600f9663364a5
Amartya Varma <amartya.varma@gmail.com>
2024-12-05 22:32:05 -0800

    f

--- import_check.py
+++
@@ -1,7 +0,0 @@
-import torch
-import torchaudio
-import transformers
-
-print(f"PyTorch version: {torch.__version__}")
-print(f"torchaudio version: {torchaudio.__version__}")
-print(f"transformers version: {transformers.__version__}")
--- upload_model.py
+++ upload_model.py
@@ -2,8 +2,8 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "models_joint/checkpoint-78"
-upload_name = "amuvarma/e2e-3-joint"
+base_repo_id = "models_joint/checkpoint-10000"
+upload_name = "amuvarma/projector-train-checkpoint-10000"
 
 
 def push_folder_to_hub(local_folder, repo_id, commit_message="Update model"):

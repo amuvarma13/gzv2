d3992e1f42cd8d465c1686eae6f42afe67b10124
Amartya Varma <amartya.varma@gmail.com>
2024-12-07 15:33:49 -0800

    f

--- upload_model.py
+++ upload_model.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 import os
 
 
-base_repo_id = "checkpoint-156"
+base_repo_id = "models/checkpoint-156"
 upload_name = "amuvarma/llm-train-tune-voiceassistant-checkpoint-156"
 
 

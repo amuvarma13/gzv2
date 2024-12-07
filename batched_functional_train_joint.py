e78f663f84f8986719ba46c80cdb25733676bf9a
Amartya Varma <amartya.varma@gmail.com>
2024-12-06 18:01:29 -0800

    f

--- batched_functional_train_joint.py
+++ batched_functional_train_joint.py
@@ -17,9 +17,9 @@ from transformers import CONFIG_MAPPING
 from transformers.models.auto.modeling_auto import MODEL_FOR_CAUSAL_LM_MAPPING
 
 dsn1 = "amuvarma/voice-assistant-200k-processed-1"
-dsn2 = "amuvarma/10k-audio-audio-contentonly"
+# dsn2 = "amuvarma/10k-audio-audio-contentonly"
 ds1 = load_dataset(dsn1, split="train")
-ds2 = load_dataset(dsn2, split="train")
+# ds2 = load_dataset(dsn2, split="train")
 
 from gzf import (
     GazelleConfig,

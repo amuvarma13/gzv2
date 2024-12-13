0cc3eeec44d9bab049bbcc14aab0467e6cc851cf
Amartya Varma <amartya.varma@gmail.com>
2024-12-13 11:01:40 -0800

    f

--- functional_train.py
+++ functional_train.py
@@ -57,7 +57,7 @@ new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]
 tokenizer.add_tokens(new_tokens)
 tokenizer.add_special_tokens({'additional_special_tokens': ['<|audio|>']})
 
-model.resize_token_embeddings(len(tokenizer))
+# model.resize_token_embeddings(len(tokenizer))
 
 
 

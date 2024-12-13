0798d0ee27f67a302f2c3e5f43a3e66da2969ee4
Amartya Varma <amartya.varma@gmail.com>
2024-12-13 02:53:35 -0800

    f

--- functional_train_joint.py
+++ functional_train_joint.py
@@ -44,7 +44,7 @@ elif torch.backends.mps.is_available():
 tokenizer = transformers.AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
 
 # 4. Add custom tokens
-number_add_tokens = 6 * 1024 + 10  # 6144 + 10 = 6154
+number_add_tokens = 7 * 4096 + 10  # 6144 + 10 = 6154
 new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]  # 6155 tokens
 tokenizer.add_tokens(new_tokens)
 tokenizer.add_special_tokens({'additional_special_tokens': ['<|audio|>']})

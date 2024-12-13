ed5f8dcd917ce64acd644a82269e7f076b0b9c3f
Amartya Varma <amartya.varma@gmail.com>
2024-12-13 03:02:11 -0800

    f

--- functional_train_joint.py
+++ functional_train_joint.py
@@ -44,8 +44,8 @@ elif torch.backends.mps.is_available():
 tokenizer = transformers.AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
 
 # 4. Add custom tokens
-number_add_tokens = 7 * 4096 + 10  # 6144 + 10 = 6154
-new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]  # 6155 tokens
+number_add_tokens = 7 * 4096 + 10 
+new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]
 tokenizer.add_tokens(new_tokens)
 tokenizer.add_special_tokens({'additional_special_tokens': ['<|audio|>']})
 
@@ -90,6 +90,9 @@ dataset = ds
 
 
 model = model.to(dtype=dtype)
+
+print(model)
+print(len(tokenizer))
 
 # First freeze all parameters
 for param in model.parameters():

f8df62df2240ba5cbcb3e809c5f576bb75c1857e
Amartya Varma <amartya.varma@gmail.com>
2024-12-13 17:46:21 -0800

    f

--- batched_functional_train_joint.py
+++ batched_functional_train_joint.py
@@ -17,7 +17,7 @@ from transformers import CONFIG_MAPPING
 from transformers.models.auto.modeling_auto import MODEL_FOR_CAUSAL_LM_MAPPING
 
 dsn1 = "amuvarma/voice-assistant-250-300k-processed"
-dsn2 = "amuvarma/va-330k-380k-snac-StTtS"
+dsn2 ="amuvarma/va-330k-380k-snac-StTtS"
 
 ds1 = load_dataset(dsn1, split="train")
 
@@ -67,8 +67,8 @@ elif torch.backends.mps.is_available():
 tokenizer = transformers.AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-3B-Instruct")
 
 # 4. Add custom tokens
-number_add_tokens = 7 * 4096 + 10  # 6144 + 10 = 6154
-new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]  # 6155 tokens
+number_add_tokens = 7 * 4096 + 10
+new_tokens = [f"<custom_token_{i}>" for i in range(0, number_add_tokens + 1)]
 tokenizer.add_tokens(new_tokens)
 tokenizer.add_special_tokens({'additional_special_tokens': ['<|audio|>']})
 
@@ -180,7 +180,7 @@ class FSDPTrainer(Trainer):
             wandb.log({"audio_loss": logs["loss"], "step": global_step})
 
 batch_total = number_processes * batch_size
-train_dataset = BatchedAlternatingDataset(filtered_ds, ds2, batch_total)
+train_dataset = BatchedAlternatingDataset(ds2, ds2, batch_total)
 
 
 

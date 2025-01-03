611004c75e706071d08b6cbd78febd3804a2a931
Amartya Varma <amartya.varma@gmail.com>
2025-01-03 23:23:56 +0000

    f

--- functional_train.py
+++ functional_train.py
@@ -37,7 +37,7 @@ elif torch.backends.mps.is_available():
     print(f"Using {device} device")
 
 
-model_id = "./mymodel"
+model_id = "amuvarma/zuck-tttts-convo-checkpoint-500-noidentity"
 
 
 config = GazelleConfig(

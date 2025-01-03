1a28c69213a5cdb0b57ef6179467440fddb52626
Amartya Varma <amartya.varma@gmail.com>
2025-01-03 13:18:23 +0000

    f

--- trainllmzucktune.py
+++ trainllmzucktune.py
@@ -74,7 +74,7 @@ print("after loading")
 # print(model)
 
 # dsn = "amuvarma/voice-assistant-200k-processed-1"
-dsn = "amuvarma/amu-zucktts-with-qaudio-total-cast-shuf"# dsn = "amuvarma/mls-eng-10k-dev-3k"
+dsn = "amuvarma/zuckqa-with-qaaudio-total-cast-snacced"# dsn = "amuvarma/mls-eng-10k-dev-3k"
 ds = load_dataset(dsn, split="train")
 # ds = ds.select(range(10000, 199999))
 def remove_short_audio(dataset, min_seconds=1.0):

3f7c87b648cd24bbe61ade29be581d650cd19778
Amartya Varma <amartya.varma@gmail.com>
2025-01-04 20:34:17 +0000

    f

--- batched_train_convo.py
+++ batched_train_convo.py
@@ -77,9 +77,9 @@ wandb.init(
 
 print(model)
 
-# dsn1 = "amuvarma/snacced-zuck-convo-sttsed-proc-no-discrepancies"
+dsn1 = "amuvarma/snacced-zuck-convo-sttsed-proc-no-discrepancies"
 # dsn2 = "amuvarma/snacced-zuck-convo-sttsed-proc-no-discrepancies"
-dsn1 = "amuvarma/qa_pairs_regular-sttsed-proc"
+# dsn1 = "amuvarma/qa_pairs_regular-sttsed-proc"
 dsn2 = "amuvarma/qa_pairs_regular-sttsed-proc"
 
 # dsn1 = "amuvarma/zuckqa-with-qaaudio-total-cast-snacced"

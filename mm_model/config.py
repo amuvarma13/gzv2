814070f8ba27e56a42baa46154bc977137cbd9ed
Amartya Varma <amartya.varma@gmail.com>
2025-01-27 10:48:52 -0800

    f

--- mm_model/config.py
+++ mm_model/config.py
@@ -7,10 +7,8 @@ class OrpheusConfig(PretrainedConfig):
     is_composition = False
     def __init__(
         self,
-        # audio_config=None,
         text_config=None,
         audio_hidden_size = 768,
-        # text_model_id=None,
         ignore_index=-100,
         audio_token_index=32000,
         vocab_size=32000,
@@ -24,10 +22,6 @@ class OrpheusConfig(PretrainedConfig):
         self.audio_token_index = audio_token_index
         self.vocab_size = vocab_size
         self.audio_hidden_size = audio_hidden_size
-
-        # self.text_model_id = text_model_id
-
-        # self.audio_config = audio_config
         self.text_config = text_config
 
         self.hidden_size = hidden_size
@@ -43,13 +37,4 @@ class OrpheusConfig(PretrainedConfig):
         elif text_config is None:
             self.text_config = CONFIG_MAPPING["llama"]()
         
-        # if isinstance(self.audio_config, dict):
-        #     audio_config["model_type"] = (
-        #         audio_config["model_type"] if "model_type" in audio_config else "wav2vec2"
-        #     )
-        #     self.audio_config = CONFIG_MAPPING[audio_config["model_type"]](**audio_config)
-        #     self.vocab_size = self.audio_config.vocab_size
-        # elif audio_config is None:
-        #     self.audio_config = CONFIG_MAPPING["wav2vec2"]()
-
         super().__init__(**kwargs)
